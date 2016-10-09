e17562c77479ba0066f3a4c4d767aeebda85191a
Tadek Teleżyński <tadekte@gmail.com>
2016-10-09 19:56:39 +0200

    add fibonacci

--- lab1/silnia.ml
+++ lab1/silnia.ml
@@ -10,9 +10,23 @@ let factorial n =
 
 (* This function takes two rectangles specified as bottom-left (first) and top-right (second) corners given by date cartesian coordinates and returns a rectangle of shared space. If the cooridnates indicate that other corners were chosen it denotes "empty" triangle which means arguments do not overlap each other *)
 let rect ((x1,y1),(x2,y2)) ((x3,y3),(x4,y4)) =
-	((max x1 x3, max y1 y3),(min x2 x4, min y2 y4))
+	((max x1 x3, max y1 y3),(min x2 x4, min y2 y4));;
 
 (* list reverse *)
 
 
 (* fibonacci *)
+let rec fib_num n =
+    match n with
+    | _ when n<1 -> 0
+    | 1 -> 1
+    | _ -> fib_num (n-1) + fib_num (n-2)
+
+(* fibonacci with tail recursion*)
+let fib_num_tail n =
+    let rec fib_help x acc1 acc2 =
+        match x with
+        | _ when x<1 -> acc1
+        | _ -> (fib_help (x-1) (acc2) (acc1+acc2))
+    in match n with
+    | _ -> fib_help n 0 1;;
