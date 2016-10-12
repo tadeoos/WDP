caf551549d116b25d6c6c9957fa6eaac7089b1b1
Tadek Teleżyński <tadekte@gmail.com>
2016-10-12 23:18:56 +0200

    etc

--- lab1/silnia.ml
+++ lab1/silnia.ml
@@ -1,3 +1,4 @@
+
 let factorial n =
 	let rec help_fact x acc =
 		match x with
@@ -35,3 +36,54 @@ let fib_num_tail n =
         | _ -> (fib_help (x-1) (acc2) (acc1+acc2))
     in match n with
     | _ -> fib_help n 0 1;;
+
+(* this function takes x and y and returns x^y in O(n) *)
+let podnies_do_potegi x y =
+    let rec pom_pod b c = if b=0 then c else pom_pod (b-1) (c*x)
+in if y<0 then -1 else pom_pod y 1;;
+
+(* and now in O(logn) *)
+let pp x y =
+    let rec fp pod wyk wyn =
+    if wyk = 0 then wyn else
+    if wyk mod 2 = 1 then fp (pod*pod) (wyk/2) (wyn*pod) else
+    fp (pod*pod) (wyk/2) wyn
+in if y<0 then -1 else fp x y 1;;
+
+(* this function takes a function nat(N) -> int(Z) and gives the maximal sum of some subset of section (1,n)*)
+let max_suma f n =
+    let rec policz g s minim wynik =
+        if g = n then (max wynik (s-minim))
+        else let noweS = s + (f (g+1)) in
+        policz (g+1) noweS (min minim noweS) (max wynik (noweS-minim))
+    in if n>0 then policz 1 (f 1) (min 0 (f 1)) (max 0 (f 1)) else -1;;
+
+let f n =
+  match n with
+  | _ when n mod 3 = 1 -> 7*n-2
+  | _ -> -n*3+7;;
+
+
+(* measure execution time; taken from http://stackoverflow.com/questions/27510037/ocaml-measure-execution-time-in-toplevel *)
+let time f x =
+      let t = Sys.time() in
+      let fx = f x in
+      Printf.printf "execution time: %fs\n" (Sys.time() -. t);
+      fx;;
+
+(* let rec average_time n res acc f x =
+  match acc with
+  | 0 -> res./.n.
+  | _ -> average_time n (res. +. time_raw (f x)) (acc-1) f x;; *)
+
+print_string "\nmax suma n=14\n";;
+time max_suma f 700;;
+
+print_string "\npodnies do potegi\n";;
+time podnies_do_potegi 16 40022;;
+time pp 16 40022;;
+
+print_string "\nfibonacii\n";;
+(* huuuge difference! *)
+time fib_num 20;;
+time fib_num_tail 100;;
