6bbdd16162c393a1f7780383334e87ca11648238
Tadek <tad@MacBook-Air-6.local>
2016-11-01 17:07:46 +0100

    fied some corner cases for razy func

--- zz1/tt/arytmetyka.ml
+++ zz1/tt/arytmetyka.ml
@@ -10,10 +10,6 @@ type wartosc = { a : float; b : float }
 
 (* niezwykly przedzial to taki ktory jest suma przedzialow od nieskonczonosci *)
 
-let rec print_list = function
-[] -> ()
-| e::l -> print_float e ; print_string " " ; print_list l
-
 let czy_zwykly w = w.a<=w.b || (w.a==neg_infinity && w.b==infinity)
 
 let skombinuj w1 w2 = [w1.a*.w2.a; w1.a*.w2.b; w1.b*.w2.b; w1.b*.w2.a]
@@ -22,18 +18,30 @@ let odwrotnosc w = {a=1./.w.b; b=1./.w.a}
 
 let get_nonzero w = if w.a==0. then w.b else w.a
 
-let sign f =
+(* let sign f =
   match f with
   | 0. -> 0.
   | _ when f > 0. -> 1.
-  | _ when f < 0. -> -1.
-(* let druk w = Printf.fprintf stdout "a=%f b=%f" w.a w.b *)
+  | _ when f < 0. -> -1. *)
 
+let sign f =
+  match f>=0. with
+  | true when f=0. -> 0.
+  | true -> 1.
+  | false -> -1.
+(* let druk w = Printf.fprintf stdout "a=%f b=%f" w.a w.b *)
+(* let _ = Printf.fprintf stdout "sign 1! %f\n" (sign 1.)
+let _ = Printf.fprintf stdout "sign 1! %f\n" (sign 4.)
+let _ = Printf.fprintf stdout "sign 0! %f\n" (sign 0.)
+let _ = Printf.fprintf stdout "sign -5! %f\n" (sign (-5.))
+let _ = Printf.fprintf stdout "sign -5! %f\n" (sign (-1.))
+let _ = Printf.fprintf stdout "sign -5! %f\n" (sign (-0.)) *)
 (* co zwracac tu?  *)
 let czy_cala w =
   if czy_zwykly w
   then {a=neg_infinity; b=infinity}
   else w
+
 let minl l =
   let rec help l acc =
     match l with
@@ -47,6 +55,16 @@ let maxl l =
     | [] -> acc
     | h::t -> if h>acc then help t h else help t acc
   in help l neg_infinity
+(*
+let srl l =
+  let rec help l n acc =
+  match l, n with
+  | h::t, 1 -> help l (n+1) h::acc
+  | h::t, 2 -> help l (n+1) h::acc
+  | _, 0 -> help l (n+1) acc
+  | _ -> acc
+  in help (List.sort compare l) 0 [] *)
+(* METODY MODUŁU *)
 
 let wartosc_dokladnosc x p =
   let delta = (x*.p)/.100.
@@ -64,7 +82,7 @@ let in_wartosc w x =
   | false -> w.a <= x || w.b >= x
 
 let min_wartosc w =
-  let _ = Printf.fprintf stdout "min war! %f %f \n" w.a w.b in
+  (* let _ = Printf.fprintf stdout "min war! %f %f \n" w.a w.b in *)
   match czy_zwykly w with
   | true -> w.a
   | false -> neg_infinity
@@ -79,55 +97,56 @@ let sr_wartosc w =
   | true -> (w.a +. w.b)/.2.
   | false -> nan
 
-(* Operacje arytmetyczne na niedokładnych wartościach. *)
-(* val plus:      wartosc -> wartosc -> wartosc *)
-
 let plus w1 w2 =
-  if czy_zwykly w1 && czy_zwykly w2
-  then {a=(w1.a +. w2.a); b=(w1.b +. w2.b)}
-  else czy_cala {a=(w1.a +. (min w2.a w2.b)); b=(w1.b +. (max w2.a w2.b))}
-
-(* val minus:     wartosc -> wartosc -> wartosc *)
-
+  match czy_zwykly w1, czy_zwykly w2 with
+  | true, false -> czy_cala {a=(w1.a +. w2.a); b=(w1.b +. w2.b)}
+  | true, true -> {a=(w1.a +. w2.a); b=(w1.b +. w2.b)}
+  | false, true -> czy_cala {a=(w1.a +. (min w2.a w2.b)); b=(w1.b +. (max w2.a w2.b))}
+  | false, false -> czy_cala {a=(w1.b +. w2.a); b=(w1.a +. w2.b)}
+  (* | true, false -> {a=w1. ;b=} *)
 let minus w1 w2 =
   if czy_zwykly w1 && czy_zwykly w2
   then {a=(w1.a -. w2.b); b=(w1.b -. w2.a)}
   else czy_cala {a=(w1.a +. (min w2.a w2.b)); b=(w1.a -. (max 0. w2.b))}
 
 
-(* val razy:      wartosc -> wartosc -> wartosc *)
-(* val podzielic: wartosc -> wartosc -> wartosc *)
+let pom_raz w1 w2 =
+  match in_wartosc w1 0. || in_wartosc w2 0. with
+  | true -> wartosc_od_do (neg_infinity) (infinity)
+  | false -> {b=max (w1.a*.w2.b) (w1.b*.w2.a); a=min (w1.a*.w2.a) (w1.b*.w2.b)}
 
-let razy w1 w2 =
-  let _ = Printf.fprintf stdout "razy! w1=%f %f; w2=%f %f \n" w1.a w1.b w2.a w2.b in
+let rec razy w1 w2 =
+  (* let _ = Printf.fprintf stdout "razy! w1=%f %f; w2=%f %f \n" w1.a w1.b w2.a w2.b in *)
   let komb = skombinuj w1 w2
-  in if czy_zwykly w1 && czy_zwykly w2
-  then {a=minl komb; b=maxl komb}
-  else {a=maxl komb; b=minl komb}
+  in match czy_zwykly w1, czy_zwykly w2 with
+  | true, true -> {a=minl komb; b=maxl komb}
+  | true, false -> {a=min (w1.a*.w2.a) (w1.b*.w2.a); b=max (w1.a*.w2.b) (w1.b*.w2.b)}
+  | false, true -> razy w2 w1
+  | false, false -> pom_raz w1 w2
 
 let pom_dziel w1 w2 =
   let p1 = w1.a/.(get_nonzero w2) in
   let p2 = w1.b/.(get_nonzero w2) in
   if w2.a==0. && w2.b==0. then wartosc_dokladna nan else
-  match sign (w1.a*.w1.b), w1.a==w1.b, sign (w2.a*.w2.b), sign (sign (w2.a)*.sign (get_nonzero w2)) with
-  | 0., true, 0., _-> wartosc_dokladna 0.
+  (* jak rozlozony jest licznik, czy jest dokladna wartoscia, jak rozlozony jest mianownik,  *)
+  match sign (w1.a*.w1.b), w1.a==w1.b, sign (w2.a*.w2.b), sign (get_nonzero w2) with
+  | 0., true, 0., _ -> wartosc_dokladna 0.
   | 0., false, 0., _ -> {a=min 0. ((sign ((w1.a+.w1.b)*.(w2.a+.w2.b)))*.infinity); b=max 0. ((sign ((w1.a+.w1.b)*.(w2.a+.w2.b)))*.infinity) }
   | 1., _, 0., 1. -> {a=min p1 p2; b=infinity}
   | 1., _, 0., -1. -> {a=neg_infinity; b=max p1 p2}
   | 1., _, -1., _ -> {a=w1.b/.w2.b; b=w1.b/.w2.a}
   | -1., _, 1., _ when sign w2.b==1.-> {a=w1.a/.w2.b; b=w1.b/.w2.b}
   | -1., _, 1., _ when sign w2.b==(-1.)-> {a=w1.b/.w2.a; b=w1.a/.w2.a}
-  | _ -> wartosc_dokladna nan
+  | _ -> let _ = Printf.fprintf stdout "auc! %f %f %f %f\n" w1.a w1.b w2.a w2.b in failwith "nielapany wyjatek"
 
 let podzielic w1 w2 =
   match in_wartosc w1 0., in_wartosc w2 0. with
   | false, false when czy_zwykly w1 && czy_zwykly w2-> razy w1 (odwrotnosc w2)
-  | false, false when czy_zwykly w1 -> pom_dziel w1 w2
-  | false, false when czy_zwykly w2 -> pom_dziel w1 w2
+  | false, false -> pom_dziel w1 w2
   | false, true -> {a=max (w1.a/.w2.a) (w1.b/.w2.a); b=(min (w1.a/.w2.b) (w1.b/.w2.b))}
   | true, true -> if List.mem 0. [w1.a;w1.b;w2.a;w2.b] then pom_dziel w1 w2 else {b=infinity; a=neg_infinity}
   | true, false -> if List.mem 0. [w1.a;w1.b;w2.a;w2.b] then pom_dziel w1 w2 else {b=infinity; a=neg_infinity}
-  | _ -> wartosc_dokladna nan
+
 (* Testy *)
 
 
--- zz1/tt/testy.ml
+++ zz1/tt/testy.ml
@@ -7,8 +7,8 @@ open Arytmetyka
 let test_podziel a b c d =
   assert ((podzielic a b) = (wartosc_od_do c d))
   ;;
-let test_warotsc a b c =
-  Printf.fprintf stdout " %f %f \n" (min_wartosc a) (max_wartosc a);
+let test_wartosc a b c =
+  (* Printf.fprintf stdout " %f %f \n" (min_wartosc a) (max_wartosc a); *)
   assert (a = (wartosc_od_do b c));;
 (* let p = druk a;; *)
 let druk a w =
@@ -16,34 +16,53 @@ let druk a w =
 
 
 let a = wartosc_od_do (-1.) 1.            (* <-1, 1> *)
-
 let b = wartosc_dokladna (-1.)            (* <-1, -1> *)
 let c = podzielic b a                     (* (-inf -1> U <1 inf) *)
 ;;
-test_warotsc c 1. (-1.);;
+assert (c = (wartosc_od_do 1. (-1.)));;
 let d = plus c a                          (* (-inf, inf) *)
 let e = wartosc_dokladna 0.               (* <0, 0> *)
 ;;
-test_warotsc e 0. (0.);;
+assert (e = (wartosc_od_do 0. (0.)));;
 let f = razy c e                          (* <0, 0> *)
 ;;
-test_warotsc f 0. (0.);;
+assert (f = (wartosc_od_do 0. (0.)));;
 let h = wartosc_dokladnosc (-10.) 50.     (* <-15, -5> *)
-let i = podzielic h e                     (* nan, przedzial pusty*)
-let j = wartosc_od_do (-6.) 5.            (* <-6, 5> *)
-let k = razy j j                          (* <-30, 36> *)
-let l = plus a b                          (* <-2, 0> *)
-let m = razy b l                          (* <0, 2> *)
-let n = podzielic l l                     (* <0, inf) *)
-let o = podzielic l m                     (* (-inf, 0) *)
-let r = minus n n                         (* (-inf, inf) *)
-let s = wartosc_dokladnosc (-0.0001) 100. (* <-0.0002, 0> *)
-let t = razy n s                          (* (-inf, 0) *)
-
-let g = podzielic j (wartosc_od_do (-4.) 3.)
 ;;
+assert (h = (wartosc_od_do (-15.) (-5.)));;
+let i = podzielic h e                     (* nan, przedzial pusty*)
+;;
+(* assert (i = (wartosc_od_do (nan) (nan)));; *)
+let j = wartosc_od_do (-6.) 5.            (* <-6, 5> *)
+;;
+assert (j = (wartosc_od_do (-6.) (5.)));;
+let k = razy j j                          (* <-30, 36> *)
+;;
+assert (k = (wartosc_od_do (-30.) (36.)));;
+let l = plus a b                          (* <-2, 0> *)
+;;
+assert (l = (wartosc_od_do (-2.) (0.)));;
+let m = razy b l                          (* <0, 2> *)
+;;
+assert (m = (wartosc_od_do (0.) (2.)));;
+let n = podzielic l l                     (* <0, inf) *)
+;;
+assert (n = (wartosc_od_do (0.) (infinity)));;
+let o = podzielic l m                     (* (-inf, 0) *)
+;;
+assert (o = (wartosc_od_do (neg_infinity) (0.)));;
+let r = minus n n                         (* (-inf, inf) *)
+;;
+assert (r = (wartosc_od_do (neg_infinity) (infinity)));;
+let s = wartosc_dokladnosc (-0.0001) 100. (* <-0.0002, 0> *)
+;;
+assert (s = (wartosc_od_do (-0.0002) (0.)));;
+let t = razy n s                          (* (-inf, 0) *)
+;;
+assert (t = (wartosc_od_do (neg_infinity) (0.)));;
+let g = podzielic j (wartosc_od_do (-4.) 3.);;
 
-druk "a" a;;
+(* druk "a" a;;
 druk "b" b;;
 druk "c" c;;
 druk "d" d;;
@@ -60,7 +79,7 @@ druk "n" n;;
 druk "o" o;;
 druk "r" r;;
 druk "s" s;;
-druk "t" t;;
+druk "t" t;; *)
 
 
 assert (in_wartosc a 0. == true);
@@ -77,28 +96,80 @@ assert ((min_wartosc n, max_wartosc n, sr_wartosc n) = (0., infinity, infinity))
 assert ((min_wartosc o, max_wartosc o, sr_wartosc o) = (neg_infinity, 0., neg_infinity));
 assert ((min_wartosc r, max_wartosc r, compare (sr_wartosc r) nan) = (neg_infinity, infinity, 0));
 assert ((min_wartosc t, max_wartosc t, sr_wartosc t) = (neg_infinity, 0., neg_infinity));
-
+;;
 (* [2;-1]/[3;5]=[0.4;-0.2] *)
-druk "x" (podzielic (wartosc_od_do 2. (-1.)) (wartosc_od_do 3. 5.));;
+(* druk "x" (podzielic (wartosc_od_do 2. (-1.)) (wartosc_od_do 3. 5.));; *)
 let x = podzielic (wartosc_od_do 2. (-1.)) (wartosc_od_do 3. 5.);;
 assert (x = wartosc_od_do (2./.5.) (-1./.5.));
 ;;
 
 (* [2;-1]/[-5;-3]=[0.2;-0.4] *)
 let test = podzielic (wartosc_od_do 2. (-1.)) (wartosc_od_do (-5.) (-3.));;
-test_warotsc test 0.2 (-0.4);;
-druk "test" test;;
+assert (test = (wartosc_od_do 0.2 (-0.4)));;
+(* druk "test" test;; *)
+
 
 (* [2;-1]+[-5;-1]=[-inf; inf] *)
-druk "t1" (plus (wartosc_od_do 2. (-1.)) (wartosc_od_do (-5.) (-1.)));;
+let t1 = plus (wartosc_od_do 2. (-1.)) (wartosc_od_do (-5.) (-1.));;
+assert (t1 = (wartosc_od_do (neg_infinity) (infinity)));;
+(* druk "t1" t1;; *)
 (* [2;-1]+[-1;1]=[1;0] *)
-druk "t2" (plus (wartosc_od_do 2. (-1.)) (wartosc_od_do (-1.) (1.)));;
+let t2 = plus (wartosc_od_do 2. (-1.)) (wartosc_od_do (-1.) (1.));;
+assert (t2 = (wartosc_od_do (1.) (0.)));;
+(* druk "t2" t2;; *)
 (* [-4;-1]+[2;-1]=[-inf;inf] *)
-druk "t4" (plus (wartosc_od_do (-4.) (-1.)) (wartosc_od_do 2. (-1.)));;
+let t4 = plus (wartosc_od_do (-4.) (-1.)) (wartosc_od_do 2. (-1.));;
+assert (t4 = (wartosc_od_do (neg_infinity) (infinity)));;
+(* druk "t4" t4;; *)
 (* [-0.5;1]+[2;-1]=[1.5;0] *)
-druk "t5" (plus (wartosc_od_do (-0.5) (1.)) (wartosc_od_do 2. (-1.)));;
+let t5 = plus (wartosc_od_do (-0.5) (1.)) (wartosc_od_do 2. (-1.));;
+assert (t5 = (wartosc_od_do (1.5) (0.)));;
+(* druk "t5" t5;; *)
 (* [-2;-0.5]+[2;-1]=[0;-1.5] *)
-druk "t6" (plus (wartosc_od_do (-2.) (-0.5)) (wartosc_od_do 2. (-1.)));;
+let t6 = plus (wartosc_od_do (-2.) (-0.5)) (wartosc_od_do 2. (-1.));;
+assert (t6 = (wartosc_od_do (0.) (-1.5)));;
+(* druk "t6" t6;; *)
 
-(* [4;-0.5]+[2;-1]=? *)
-druk "t7" (plus (wartosc_od_do (4.) (-0.5)) (wartosc_od_do 2. (-1.)));;
+(* [4;-0.5]+[2;-1]=[-inf;inf] *)
+let t7 = plus (wartosc_od_do (4.) (-0.5)) (wartosc_od_do 2. (-1.));;
+assert (t7 = (wartosc_od_do (neg_infinity) (infinity)));;
+(* druk "t7" t7;; *)
+(* [4;-0.5]-[2;-1]=[-inf;inf] *)
+let t7 = minus (wartosc_od_do (4.) (-0.5)) (wartosc_od_do 2. (-1.));;
+assert (t7 = (wartosc_od_do (neg_infinity) (infinity)));;
+(* druk "t8" t7;; *)
+
+(* [3;5]*[4;-2]=[12, -6] *)
+let t7 = razy (wartosc_od_do (3.) (5.)) (wartosc_od_do 4. (-2.));;
+(* druk "t9" t7;; *)
+assert (t7 = (wartosc_od_do (12.) (-6.)));;
+
+(* [5;3]*[2;-1]=[neg_infinity, infinity] *)
+let t7 = razy (wartosc_od_do (5.) (3.)) (wartosc_od_do 2. (-1.));;
+(* druk "t10" t7;; *)
+assert (t7 = (wartosc_od_do (neg_infinity) (infinity)));;
+
+
+(* [4;-0.5]*[2;-1]=[0.5;-1] *)
+let t7 = razy (wartosc_od_do (4.) (-0.5)) (wartosc_od_do 2. (-1.));;
+(* druk "t11" t7;; *)
+assert (t7 = (wartosc_od_do (0.5) (-1.)));;
+
+(* [1;0]*[2;0]=[-inf;inf] *)
+let t7 = razy (wartosc_od_do (1.) (0.)) (wartosc_od_do 2. 0.);;
+assert (t7 = (wartosc_od_do (neg_infinity) (infinity)));;
+(* [-0.5;0.5]*[-0.2;0.6]=? *)
+let t7 = razy (wartosc_od_do (-0.5) (0.5)) (wartosc_od_do (-0.2) (0.6));;
+assert (t7 = (wartosc_od_do (-0.3) (0.3)));;
+
+(* [15;-20]*[4;-6]= [60;-80] *)
+let t7 = razy (wartosc_od_do (15.) (-20.)) (wartosc_od_do 4. (-6.));;
+assert (t7 = (wartosc_od_do (60.) (-80.)));;
+
+let t7 = razy (wartosc_od_do (0.) (0.)) (wartosc_od_do 4. (-6.));;
+assert (t7 = (wartosc_od_do (0.) (0.)));;
+
+let t7 = razy (wartosc_od_do (0.) (2.)) (wartosc_od_do (-10.) (-6.));;
+assert (t7 = (wartosc_od_do (-20.) (0.)));;
+
+Printf.fprintf stdout "\n--- ALL tests PASSED ---\n";;
