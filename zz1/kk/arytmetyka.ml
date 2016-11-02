1b6180bc86674290263ba70ac6cbda0b49809a51
kkanska <katarzyna.m.kanska@gmail.com>
2016-11-02 02:43:21 +0100

    fixed podzielic

--- zz1/kk/arytmetyka.ml
+++ zz1/kk/arytmetyka.ml
@@ -39,7 +39,7 @@ let wartosc_dokladna x = if (x == infinity) || (x == neg_infinity) || (x == nan)
 let in_wartosc w x = if (w.poczatek <= w.koniec) then
 						(w.poczatek <= x) && (x <= w.koniec)
 					else
-						(x < w.koniec) || (w.poczatek < x)
+						(x <= w.koniec) || (w.poczatek <= x)
 ;;
 
 (* min_wartosc w = najmniejsza możliwa wartość w,   *)
@@ -68,21 +68,22 @@ let sr_wartosc w = if (w.poczatek <= w.koniec) && (max_wartosc w != infinity) th
 
 (* Operacje arytmetyczne na niedokładnych wartościach. *)
 let plus w v =
-	let czy_antyprzedzial z = (w.poczatek > w.koniec) in
+	let czy_antyprzedzial z = (z.poczatek > z.koniec) in
 	if (czy_antyprzedzial w) && (czy_antyprzedzial v) then
 		{poczatek = neg_infinity; koniec = infinity} (* wynikiem jest cala prosta rzeczywista *)
 	else
 		if (czy_antyprzedzial w) || (czy_antyprzedzial v) then
 			if ((w.poczatek +. v.poczatek) <= (w.koniec +. v.koniec)) then
-				{poczatek = neg_infinity; koniec = infinity}  (* wynikiem jest cala prosta rzeczywista *)
+				{poczatek = neg_infinity; koniec = infinity} (* wynikiem jest cala prosta rzeczywista *)
 			else
 				{poczatek = (w.poczatek +. v.poczatek); koniec = (w.koniec +. v.koniec)} (* wynikiem jest antyprzedzial *)
 		else
-			{poczatek = (min_wartosc w +. min_wartosc v); koniec = (max_wartosc w +. max_wartosc v)}  (* wynikiem jest przedzial lub cala prosta, jesli ktorys z przedzialow byl cala prosta *)
+			{poczatek = (min_wartosc w +. min_wartosc v); koniec = (max_wartosc w +. max_wartosc v)}
+			(* wynikiem jest przedzial lub cala prosta, jesli ktorys z przedzialow byl cala prosta *)
 ;;
 
 let minus w v =
-	let czy_antyprzedzial z = (w.poczatek > w.koniec) in
+	let czy_antyprzedzial z = (z.poczatek > z.koniec) in
 	if (czy_antyprzedzial w) && (czy_antyprzedzial v) then
 		{poczatek = neg_infinity; koniec = infinity}  (* wynikiem jest cala prosta rzeczywista *)
 	else
@@ -90,55 +91,109 @@ let minus w v =
 			if ((w.poczatek -. v.koniec) <= (w.koniec -. v.poczatek)) then
 				{poczatek = neg_infinity; koniec = infinity}  (* wynikiem jest cala prosta rzeczywista *)
 			else
-				{poczatek = (w.poczatek -. v.koniec) ; koniec = (w.koniec -. v.poczatek)} (* wynikiem jest antyprzedzial *)
+				{poczatek = (w.poczatek -. v.koniec); koniec = (w.koniec -. v.poczatek)} (* wynikiem jest antyprzedzial *)
 		else
-			{poczatek = (min_wartosc w -. max_wartosc v) ; koniec = (max_wartosc w -. min_wartosc v)} (* wynikiem jest przedzial lub cala prosta, jesli ktorys z przedzialow byl cala prosta *)
+			{poczatek = (min_wartosc w -. max_wartosc v); koniec = (max_wartosc w -. min_wartosc v)}
+			(* wynikiem jest przedzial lub cala prosta, jesli ktorys z przedzialow byl cala prosta *)
 ;;
 
-(* ponizej moze miec sens zamienic ify na match, bo i tak chce wiedziec ktora wartosc jest przedzialem, a ktora antyprzedzialem *)
 let razy w v =
-	let czy_przedzial z = (w.poczatek <= w.koniec) in
+	let czy_przedzial z = (z.poczatek <= z.koniec) in
 	if (czy_przedzial w) && (czy_przedzial v) then
-		{poczatek = min (w.poczatek *. v.poczatek) (min (w.poczatek *. v.koniec) (min (w.koniec *. v.poczatek) (w.koniec *. v.koniec)))
-		;
-		koniec  = max (w.poczatek *. v.poczatek) (max (w.poczatek *. v.koniec) (max (w.koniec *. v.poczatek) (w.koniec *. v.koniec)))}
-		(* wynikiem jest przedzial lub cala prosta, jesli ktorys z przedzialow byl cala prosta *)
+        if (w.koniec == infinity) || (v.koniec == infinity) then
+            {poczatek = neg_infinity; koniec = infinity} (* wynikiem jest cala prosta rzeczywista *)
+        else
+            {poczatek = min (w.poczatek *. v.poczatek) (min (w.poczatek *. v.koniec) (min (w.koniec *. v.poczatek) (w.koniec *. v.koniec)));
+		    koniec  = max (w.poczatek *. v.poczatek) (max (w.poczatek *. v.koniec) (max (w.koniec *. v.poczatek) (w.koniec *. v.koniec)))}
+		    (* wynikiem jest przedzial *)
 	else
 		if (czy_przedzial w) || (czy_przedzial v) then
 			if ((czy_przedzial w) && (in_wartosc w 0.)) || ((czy_przedzial v) && (in_wartosc v 0.)) then
-				{poczatek = neg_infinity; koniec = infinity}  (* wynikiem jest cala prosta rzeczywista *)
+				if ((czy_przedzial w) && (w.poczatek = w.koniec)) || ((czy_przedzial v) && (v.poczatek = v.koniec)) then
+					{poczatek = 0.; koniec = 0.} (* wynikiem jest wartosc dokladna rowna zero *)
+				else
+					{poczatek = neg_infinity; koniec = infinity}  (* wynikiem jest cala prosta rzeczywista *)
 			else
-				if (czy_przedzial w) then
-					if (w.koniec < 0.) then
-						{poczatek = min (v.poczatek *. w.poczatek) (v.poczatek *. w.koniec)
-						;
-						koniec = max (v.koniec *. w.poczatek) (v.koniec *. w.koniec)}
-						(* wynikiem jest antyprzedzial *)
-					else
-						{poczatek = min (v.koniec *. w.poczatek) (v.koniec *. w.koniec)
-						;
+				if czy_przedzial w then
+					if w.koniec < 0. then
+						{poczatek = min (v.koniec *. w.poczatek) (v.koniec *. w.koniec);
 						koniec = max (v.poczatek *. w.poczatek) (v.poczatek *. w.koniec)}
 						(* wynikiem jest antyprzedzial *)
+					else
+						{poczatek = min (v.poczatek *. w.poczatek) (v.poczatek *. w.koniec);
+						koniec = max (v.koniec *. w.poczatek) (v.koniec *. w.koniec)}
+						(* wynikiem jest antyprzedzial *)
 				else
-					if (v.koniec < 0.) then
-						{poczatek = min (w.poczatek *. v.poczatek) (w.poczatek *. v.koniec)
-						;
+					if v.koniec < 0. then
+						{poczatek = min (w.poczatek *. v.poczatek) (w.poczatek *. v.koniec);
 						koniec = max (w.koniec *. v.poczatek) (w.koniec *. v.koniec)}
 						(* wynikiem jest antyprzedzial *)
 					else
-						{poczatek = min (w.koniec *. v.poczatek) (w.koniec *. v.koniec)
-						;
+						{poczatek = min (w.koniec *. v.poczatek) (w.koniec *. v.koniec);
 						koniec = max (w.poczatek *. v.poczatek) (w.poczatek *. v.koniec)}
 						(* wynikiem jest antyprzedzial *)
 		else
 			if (in_wartosc w 0.) || (in_wartosc v 0.) then
 				{poczatek = neg_infinity; koniec = infinity}  (* wynikiem jest cala prosta rzeczywista *)
 			else
-				{poczatek = max (w.poczatek *. v.poczatek) (w.koniec *. v.koniec)
-				;
-				koniec  = min (w.poczatek *. v.poczatek) (w.koniec *. v.koniec)}
-				(* wynikiem jest przedzial *)
+				{poczatek = min (w.poczatek *. v.poczatek) (w.koniec *. v.koniec);
+				koniec  = max (w.poczatek *. v.koniec) (w.koniec *. v.poczatek)}
+				(* wynikiem jest antyprzedzial *)
 ;;
 
-(* chwilowo... *)
-let podzielic w v = {poczatek = 0. ; koniec = 0.}
+let podzielic w v =
+	let czy_antyprzedzial z = (z.poczatek > z.koniec)
+	and czy_przedzial z = (z.poczatek <= z.koniec) in
+	if (czy_antyprzedzial w) && (czy_antyprzedzial v) then
+		{poczatek = neg_infinity; koniec = infinity}  (* wynikiem jest cala prosta rzeczywista *)
+	else
+        if czy_przedzial w then
+            match (in_wartosc w 0.), (in_wartosc v 0.) with
+            | true, true -> {poczatek = neg_infinity; koniec = infinity} (* wynikiem jest cala prosta rzeczywista *)
+            | false, true ->    if czy_przedzial v then
+                                    if w.koniec < 0. then
+                                        {poczatek = (w.koniec /. v.poczatek); koniec = (w.koniec /. v.koniec)}
+                                        (* wynikiem jest antyprzedzial *)
+                                    else
+                                        {poczatek = (w.poczatek /. v.koniec); koniec = (w.poczatek /. v.poczatek)}
+                                        (* wynikiem jest antyprzedzial *)
+                                else
+                                    match (w.poczatek < 0.), (v.poczatek < 0.) with
+                                    | true, true ->  if (w.poczatek /. v.koniec) >= (w.koniec /. v.poczatek) then
+                                                             {poczatek = neg_infinity; koniec = infinity}
+                                                             (* wynikiem jest cala prosta rzeczywista *)
+                                                        else
+                                                            {poczatek = (w.koniec /. v.poczatek); koniec = (w.poczatek /. v.koniec)}
+                                                            (* wynikiem jest antyprzedzial *)
+
+                                    | true, false -> if (w.poczatek /. v.poczatek) <= (w.koniec /. v.koniec) then
+                                                             {poczatek = neg_infinity; koniec = infinity} (* wynikiem jest cala prosta rzeczywista *)
+                                                        else
+                                                            {poczatek = (w.poczatek /. v.poczatek); koniec = (w.koniec /. v.koniec)}
+                                                            (* wynikiem jest antyprzedzial *)
+
+                                    | false, true -> if (w.koniec /. v.koniec) <= (w.poczatek /. v.poczatek) then
+                                                             {poczatek = neg_infinity; koniec = infinity}
+                                                             (* wynikiem jest cala prosta rzeczywista *)
+                                                        else
+                                                            {poczatek = (w.koniec /. v.koniec); koniec = (w.poczatek /. v.poczatek)}
+                                                            (* wynikiem jest antyprzedzial *)
+
+                                    | false, false ->   if (w.koniec /. v.poczatek) >= (w.poczatek /. v.koniec) then
+                                                             {poczatek = neg_infinity; koniec = infinity} (* wynikiem jest cala prosta rzeczywista *)
+                                                        else
+                                                            {poczatek = (w.poczatek /. v.koniec); koniec = (w.koniec /. v.poczatek)}
+                                                            (* wynikiem jest antyprzedzial *)
+            | _, false ->   {poczatek = min (w.poczatek /. v.poczatek) (min (w.poczatek /. v.koniec) (min (w.koniec /. v.poczatek) (w.koniec /. v.koniec)));
+                            koniec  = max (w.poczatek /. v.poczatek) (max (w.poczatek /. v.koniec) (max (w.koniec /. v.poczatek) (w.koniec /. v.koniec)))}
+                            (* wynikiem jest przedzial *)
+        else
+            if (in_wartosc v 0.) then
+                {poczatek = neg_infinity; koniec = infinity} (* wynikiem jest cala prosta rzeczywista *)
+            else
+                if (min (w.koniec /. v.poczatek) (w.koniec /. v.koniec)) <= (max (w.poczatek /. v.poczatek) (w.poczatek /. v.koniec)) then
+                    {poczatek = neg_infinity; koniec = infinity} (* wynikiem jest cala prosta rzeczywista *)
+                else
+                    {poczatek = (min (w.koniec /. v.poczatek) (w.koniec /. v.koniec));  koniec = (max (w.poczatek /. v.poczatek) (w.poczatek /. v.koniec))}
+                    (* wynikiem jest antyprzedzial *)
+;;
